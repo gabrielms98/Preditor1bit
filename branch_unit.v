1a6e8fd59310cd535d5de8913748abdb5ee496e4
Gabriel M Silva <gmbiel1@gmail.com>
2019-04-22 21:34:00 -0300

    é tetra

--- branch_unit.v
+++ branch_unit.v
@@ -39,32 +39,32 @@ module branch_unit(
         Wrt<=1'd0;
         Wrp<=1'd0;
         flush<=1'd0;
-      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd0)&&(c==1'd0))
+      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd0)&&(c==1'd0)) begin
         muxpc<=1'd0;
         Wrt<=1'd1;
         Wrp<=1'd1;
         flush<=1'd0;
-      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd0)&&(c==1'd1))
+      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd0)&&(c==1'd1)) begin
         muxpc<=1'd3;
         Wrt<=1'd1;
         Wrp<=1'd1;
         flush<=1'd1;
-      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd1)&&(c==1'd0)&&(Pd==1'd0))
+      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd1)&&(c==1'd0)&&(Pd==1'd0)) begin
         muxpc<=1'd0;
         Wrt<=1'd0;
         Wrp<=1'd0;
         flush<=1'd0;
-      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd1)&&(c==1'd1)&&(Pd==1'd1))
+      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd1)&&(c==1'd1)&&(Pd==1'd1)) begin
         muxpc<=1'd0;
         Wrt<=1'd0;
         Wrp<=1'd0;
         flush<=1'd0;
-      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd1)&&(c==1'd1)&&(Pd==1'd0))
+      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd1)&&(c==1'd1)&&(Pd==1'd0)) begin
         muxpc<=1'd3;
         Wrt<=1'd0;
         Wrp<=1'd1;
         flush<=1'd1;
-      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd1)&&(c==1'd0)&&(Pd==1'd1))
+      end else if((b==1'd1)&&(H==1'd0)&&(Hd==1'd1)&&(c==1'd0)&&(Pd==1'd1)) begin
         muxpc<=1'd2;
         Wrt<=1'd0;
         Wrp<=1'd1;
